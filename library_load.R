6b727a30b6248ba5e0724fa717bb8065371a6cbc
jihong <jihong-zhang@uiowa.edu>
2023-06-16 18:57:33 +0800

    change the read in data

--- library_load.R
+++ library_load.R
@@ -9,11 +9,13 @@ library_load <-
       "shinyWidgets", # for widgets like progress bar
       "DT", # for data table
       "glmmTMB", # for co-variate model
+      "gtheory", # for gtheory
       "lme4", # for lme modeling
       "markdown", # for better markdown format
       "MASS",
       "sjmisc", # check nested/crossed structure
       "tidyverse", # for better markdown format
+      "stringr",
       "parallel" # bootstrapping parallel computing
     )
   }
